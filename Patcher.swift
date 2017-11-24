356e57f743d18a22b6b7e353efe7f0fe9509b2e4
Alexey Faizullov <alex.fuzza@gmail.com>
2017-11-24 22:34:51 +0200

    WIP project DSL

--- Patcher.swift
+++ Patcher.swift
@@ -19,6 +19,47 @@ let outputFolder = "$(BUILT_PRODUCTS_DIR)/$(FRAMEWORKS_FOLDER_PATH)/"
 
 let scriptBody = "/usr/local/bin/carthage copy-frameworks"
 
+enum Dependency {
+  case carthage(String)
+}
+
+class Project {
+  var name: String
+  var testTarget: String
+  
+  var dependencies: [Dependency]
+  var testDependencies: [Dependency]
+  
+  var resolvedDependencies: [Dependency] {
+    return dependencies
+  }
+  
+  var resolvedTestDependencies: [Dependency] {
+    return dependencies + testDependencies
+  }
+  
+  init(name: String,
+       testTarget: String? = nil,
+       dependencies: [Dependency] = [],
+       testDependencies: [Dependency] = []) {
+    self.name = name
+    self.testTarget = testTarget ?? name + "Tests"
+    self.dependencies = dependencies
+    self.testDependencies = testDependencies
+  }
+}
+
+let Sample = Project(
+  name: "Sample",
+  dependencies: [
+    .carthage("RxSwift"),
+    .carthage("RxCocoa")
+  ],
+  testDependencies: [
+    .carthage("RxTest"),
+    .carthage("RxBlocking")
+  ])
+
 //do {
   let project = try! XcodeProj(path: sampleProjectPath)
   print(project)
@@ -38,7 +79,7 @@ let scriptBody = "/usr/local/bin/carthage copy-frameworks"
     let inputPaths = dependencies.map { inputFolder + $0 + ".framework" }
     let outputPaths = dependencies.map { outputFolder + $0 + ".framework" }
     
-    let reference = project.pbxproj.generateUUID(for: PBXShellScriptBuildPhase.self)
+    let reference = pbxproj.generateUUID(for: PBXShellScriptBuildPhase.self)
     
     let scriptPhase = PBXShellScriptBuildPhase(reference: reference,
                                                name: "Integrator",
